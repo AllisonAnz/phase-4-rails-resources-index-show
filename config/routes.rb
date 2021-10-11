c6f7aeded47326933e40d6af54c119e8b0653219
AllisonAnz <AllisonAnzalone@gmail.com>
2021-10-11 16:53:29 -0500

    Completed Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # instead of writing out all the methods by hand
+      # get '/birds', to: 'birds#index'
+      # get '/birds/:id', to: 'birds#show'
+
+  # We can use resources, to get access to all RESTful routes 
+  # resources :birds
+
+  # We want to be specific to the routes we want to define
+  # in this case index and create 
+  resources :birds, only: [:index, :show]
 end
+
+# Generate a controller
+# rails g controller birds --no-test-framework
+# in the controller you define all your controller methods
